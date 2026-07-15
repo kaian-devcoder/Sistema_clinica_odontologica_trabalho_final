cd02be99ecfac747778cb21b5c9b249432654535
kaian-devcoder <kaiancarvalho54@gmail.com>
2026-07-15 18:03:02 -0300

    Fazando função de alterar paciente, do menu CRUD

--- pacientes.py
+++ pacientes.py
@@ -59,3 +59,50 @@ def listar_pacientes(dados, pausar_no_final=True): #Essa função tá recebendo 
 
             print(f'\nPaciente cadastrado com o código {paciente["codigo"]}')
             pausar()
+
+
+def alterar_paciente(dados):
+    if not dados["pacientes"]:           #verificando se existe pacientes, se estiver vazia, a função vai ser encerrada lá no ( return - nesse contexto ele apenas impede que a função continue)
+        print("\nNenhum paciente cadastrado. ")
+        pausar()
+        return
+    listar_pacientes(dados, False)    #aqui é para listar os pacientes que estão em (dados -> .JSON), o false serve para não pausar no final, então não precisa clicar no ENTER  
+
+    codigo = ler_inteiro("\nCódigo do paciente que será alterado: ")
+    paciente = buscar_por_codigo(dados["pacientes"], codigo)
+    if not paciente:   #Se nenhum paciente não for encontrado, o print do erro vai ser impresso!
+        print("Paciente não encontrado. ")
+        pausar()
+        return
+    
+    print ("\n Deixe o campo vazio para manter o valor atual.")
+
+    novo_nome = input(f'Nome [{paciente["nome"]}]: ').strip()
+    novo_telefone = input(f'Telefone [{paciente["telefone"]}]: ').strip()
+
+    if novo_nome:
+        paciente["nome"] = novo_nome
+    if novo_telefone:
+        if novo_telefone.isdigit() and len(novo_telefone) >=8:
+            paciente["telefone"] = novo_telefone
+        else:
+            print("Telefone inválido. O telefone anterior foi mantido.")
+
+
+    print("\n1. Particular")
+    print("2. Convênio")
+    print("0. Manter o tipo atual")
+
+    while True: 
+        opcao = ler_inteiro("Tipo de atendimento: ")
+        if opcao in (0, 1, 2):
+            break
+        print("Escolha 0, 1 ou 2.")
+    if opcao == 1:
+        paciente["tipo_atendimento"] =  "Particular"
+    elif opcao ==2:
+        paciente["tipo_atendimento"] = "Convênio"
+
+    salvar_dados(dados)
+    print("Paciente alterado com sucesso. ")
+    pausar()
